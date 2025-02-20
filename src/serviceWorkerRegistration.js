3d5d17161da095b04b5b0a75eda9e707a6dbed44
Cristina <cristinaguzman@live.cl>
2025-02-19 22:09:06 -0300

    pwa

---
+++ src/serviceWorkerRegistration.js
@@ -0,0 +1,26 @@
+export function register() {
+    if ('serviceWorker' in navigator) {
+      window.addEventListener('load', () => {
+        navigator.serviceWorker
+          .register('/service-worker.js')
+          .then(registration => {
+            console.log('ServiceWorker registrado con éxito:', registration);
+          })
+          .catch(error => {
+            console.log('Error al registrar el ServiceWorker:', error);
+          });
+      });
+    }
+  }
+  
+  export function unregister() {
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker.ready
+        .then(registration => {
+          registration.unregister();
+        })
+        .catch(error => {
+          console.error(error.message);
+        });
+    }
+  }
